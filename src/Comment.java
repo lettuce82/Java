e6071191b7a38a48621c22aa31a8ce8f5c74a7dd
lettuce <chaesanghui142@gmail.com>
2023-12-05 15:24:07 +0900

    12/5 - 주석, 출력문

---
+++ src/Comment.java
@@ -0,0 +1,14 @@
+public class Comment {
+    
+    //psvm + Tap키 -> main 메서드 자동 생성
+    public static void main(String[] args) {
+        //sout + Tap키 -> 출력문 자동 생성
+        System.out.println("hello java1"); //hello jav1을 출력
+        //System.out.println("hello java2");
+
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+         */
+    }
+}
