3b5f2a3375ae6c31334a10f25c0ad0acafcc62ee
lettuce <chaesanghui142@gmail.com>
2023-12-07 11:11:52 +0900

    12/7 - 백준문풀(조건문)

---
+++ src/backJoon/cond/Ex5.java
@@ -0,0 +1,12 @@
+package backJoon.cond;
+
+import java.util.Scanner;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+        int result;
+    }
+}
