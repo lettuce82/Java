e68757e3eca333b2c78b49a52376d90e8d26a726
lettuce <chaesanghui142@gmail.com>
2023-12-07 15:54:31 +0900

    12/7 - do while문

---
+++ src/loop/While_1.java
@@ -0,0 +1,13 @@
+package loop;
+
+public class While_1 {
+    public static void main(String[] args) {
+        int count = 0;
+        count = count +1;
+        System.out.println("count = " + count);
+        count = count +1;
+        System.out.println("count = " + count);
+        count = count +1;
+        System.out.println("count = " + count);
+    }
+}
